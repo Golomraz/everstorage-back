3586dc5fa860dd55db633bbdba263f7be0f31051
Vitalii Kulbed <vitalik.klbd@gmail.com>
2023-12-23 22:27:52 +0300

    add cors url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:4200'],
+    origin: ['http://localhost:3000', 'http://localhost:4200', 'https://golomraz.github.io'],
     methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT'],
   });
   await app.listen(3000);
